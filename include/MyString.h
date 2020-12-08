a8e548afeac23869695127fe930fba34e66f63ea
jswark <swark.a.j@gmail.com>
2020-12-08 16:57:50 +0300

    class MyString

---
+++ include/MyString.h
@@ -0,0 +1,40 @@
+// Copyright 2020 jswark
+#ifndef INCLUDE_MYSTRING_H_
+#define INCLUDE_MYSTRING_H_
+#include <iostream>
+
+class MyString {
+ public:
+   char* m_Str = new char;
+
+  explicit MyString(const char* = nullptr);
+  explicit MyString(const std::string& str);
+  MyString(const MyString&); // конструктор копирования.
+  MyString(MyString&&); // конструктор переноса.
+  ~MyString(); // деструктор.
+  size_t length() const; // количество символов (длина строки).
+  char* get() const;
+
+  MyString operator+(const MyString &) const;
+  MyString operator-(const MyString &) const;
+  MyString operator*(int) const;
+
+  MyString& operator=(const MyString &); // копирующее присваивание.
+  MyString& operator=(MyString &&); // перемещающее присваивание.
+
+  bool operator==(const MyString&) const; // сравнение на равенство.
+  bool operator!=(const MyString& str) const; // сравнение на неравенство.
+  bool operator>(const MyString& str) const; // лексографическое сравнение .
+  bool operator<(const MyString& str) const; // лексографическое сравнение.
+  bool operator>=(const MyString& str) const; // лексографическое сравнение.
+  bool operator<=(const MyString& str) const; // лексографическое сравнение.
+
+  MyString& operator!(); // у латинских букв меняется регистр.
+  char operator[](int) const; // доступ к символу по индексу.
+  int operator()(const char*); // поиск подстроки.
+
+  friend std::ostream &operator>>(std::ostream&, MyString&);
+  friend std::istream &operator>>(std::istream&, MyString&); // запись в поток.
+};
+
+#endif // INCLUDE_MYSTRING_H_
---
+++ src/MyString.cpp
@@ -0,0 +1,141 @@
+// Copyright 2020 jswark
+#include "MyString.h"
+#include <cstring>
+#include <string>
+#include <ctype.h>
+
+MyString::MyString(const char* str) {
+  if (str != nullptr) {
+    size_t len = std::strlen(str) + 1;
+    this->m_Str = new char[len];
+    std::strncpy(this->m_Str, str, len);
+  }
+}
+
+MyString::MyString(const std::string& str) {
+  size_t len = str.length() + 1;
+  this->m_Str = new char[len];
+  std::strncpy(this->m_Str, str.c_str(), len);
+}
+
+MyString::MyString(const MyString& new_str) {
+  size_t len = new_str.length() + 1;
+  this->m_Str = new char[len];
+  std::strncpy(this->m_Str, new_str.m_Str, len);
+}
+
+MyString::MyString(MyString&& str) {
+  size_t len = str.length() + 1;
+  this-> m_Str = new char[len];
+  std::strncpy(this->m_Str, str.m_Str, len);
+}
+
+MyString::~MyString() {
+  delete this->m_Str;
+}
+
+size_t MyString::length() const {
+  return strlen(this->m_Str);
+}
+
+char* MyString::get() const {
+  return this->m_Str;
+}
+
+MyString MyString::operator+(const MyString& new_str) const {
+  size_t len1 = new_str.length();
+  size_t len2 = this->length();
+  char* tmp_str = new char[len1 + len2 + 1];
+  strncat(tmp_str, this->m_Str, len2);
+  strncat(tmp_str, new_str.m_Str, len1);
+
+  return MyString(tmp_str);
+}
+
+MyString MyString::operator-(const MyString& new_str) const {
+  std::string tmp(this->get());
+  size_t len = new_str.length();
+  for (size_t i = 0; i < len; ++i) {
+    while (tmp.find(new_str.m_Str[i]) != -1)
+      tmp.erase(tmp.find(new_str.m_Str[i]), 1);
+  }
+
+  return MyString(tmp);
+}
+
+MyString MyString::operator*(int k) const {
+  std::string tmp;
+  for (unsigned int i = 0; i < k; ++i)
+    tmp.append(this->m_Str);
+
+  return MyString(tmp);
+}
+
+MyString& MyString::operator=(const MyString& new_str) {
+    delete(this->m_Str);
+    size_t len = new_str.length() + 1;
+    char* tmp = new char[len];
+    std::strncpy(tmp, new_str.get(), len);
+
+    return *this;
+}
+
+MyString& MyString::operator=(MyString&& new_str) {
+  delete(this->m_Str);
+  this->m_Str = new_str.get();
+  new_str.m_Str = nullptr;
+
+  return *this;
+}
+
+bool MyString::operator==(const MyString& str) const {
+  return !(strcmp(this->m_Str, str.get()));
+}
+
+bool MyString::operator!=(const MyString& str) const {
+  return (strcmp(this->m_Str, str.get()));
+}
+
+bool MyString::operator>(const MyString& str) const {
+  return (strcmp(this->m_Str, str.get()) > 0);
+}
+
+bool MyString::operator<(const MyString& str) const {
+  return (strcmp(this->m_Str, str.get()) < 0);
+}
+
+bool MyString::operator>=(const MyString& str) const {
+  return (strcmp(this->m_Str, str.get()) >= 0);
+}
+
+bool MyString::operator<=(const MyString& str) const {
+  return (strcmp(this->m_Str, str.get()) <= 0);
+}
+
+MyString& MyString::operator!() {
+  unsigned int i = 0;
+  while (i != this->length()) {
+    this->m_Str[i] = char(toupper(this->m_Str[i]));
+    i++;
+  }
+
+  return *this;
+}
+
+char MyString::operator[](int i) const {
+  return this->m_Str[i];
+}
+
+int MyString::operator()(const char* str) {
+  std::string tmp = this->m_Str;
+
+  return tmp.find(str);
+}
+
+std::ostream &operator>>(std::ostream& os, MyString& new_str) {
+  return os << new_str.get();
+}
+
+std::istream &operator>>(std::istream& is, MyString& new_str) {
+  return is >> new_str.get();
+}
